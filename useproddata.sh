bca9cb855c5e91448e9e78c4432a2d3260ec38c8
Johannes Filter <hi@jfilter.de>
2021-01-31 17:34:13 +0100

    adapt script to use prod data

--- useproddata.sh
+++ useproddata.sh
@@ -4,7 +4,7 @@ set -x
 
 # loads data from the production
 
-ssh rg "./backupdata.sh"
-scp rg:/root/backupdata.json .
-scp -r rg:/root/media .
-docker-compose run --rm manage bash -c "./manage.py reset_db --noinput && ./manage.py migrate && ./manage.py loaddata backupdata.json"
+ssh rg "./exportjson.sh"
+scp rg:/root/data.json .
+scp -r rg:/var/www/rg-media media
+docker-compose run --rm manage bash -c "./manage.py reset_db --noinput && ./manage.py migrate && ./manage.py loaddata data.json"

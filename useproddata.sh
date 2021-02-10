b49469adfa63300fffd3352415c499c107c31bdf
Johannes Filter <hi@jfilter.de>
2021-02-10 01:06:00 +0100

    use backups for rev

--- useproddata.sh
+++ useproddata.sh
@@ -3,8 +3,5 @@ set -e
 set -x
 
 # loads data from the production
-
-ssh rg "./exportjson.sh"
-scp rg:/root/data.json .
-scp -r rg:/var/www/rg-media media
-docker-compose run --rm manage bash -c "./manage.py reset_db --noinput && ./manage.py migrate && ./manage.py loaddata data.json"
+rsync -rv rg:/root/dokku-backups/ misc/backups
+docker-compose run --rm manage ./manage.py dbrestore

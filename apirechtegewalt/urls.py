096e29e07ab133a3ebc8b2c76aa74c25431594ff
Johannes Filter <hi@jfilter.de>
2026-02-23 23:34:00 +0100

    Serve media files in production via Django
    The static() helper only serves media in DEBUG mode. Add an explicit
    URL pattern so uploaded media (images, renditions) is served in all
    environments.

--- apirechtegewalt/urls.py
+++ apirechtegewalt/urls.py
@@ -2,14 +2,14 @@
 apirechtegewalt URL Configuration.
 """
 from django.contrib import admin
-from django.urls import include
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("content/", include("apirechtegewalt.cms.urls")),
     path("", include("apirechtegewalt.main.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
